6353a49c8f4250ab6f3195b932e5462d879bef4f
Vadym Lazariev <vadymlazariev@gmail.com>
2020-08-26 14:29:51 +0300

    Launch the basic crud app with hooks

---
+++ src/forms/AddUserForm.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+
+const AddUserForm = (props) => {
+  const initialFormState = {id: null, name: '', username: ''};
+  const [user, setUser] = useState(initialFormState);
+
+  const handleInputChange = event => {
+    const {name, value} = event.target;
+    setUser({...user, [name]: value});
+  };
+
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        if ((!user.name || !user.username)) {
+          return;
+        }
+
+        props.addUser(user);
+        setUser(initialFormState);
+      }}
+    >
+      <label>Name</label>
+      <input onChange={handleInputChange} type="text" name="name"/>
+      <label>Username</label>
+      <input onChange={handleInputChange} type="text" name="username"/>
+      <button>Add new user</button>
+    </form>
+  );
+};
+
+export default AddUserForm;
